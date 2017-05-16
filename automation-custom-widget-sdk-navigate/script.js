733a5862115af1f7d84471160c351d8dd9ab1786
harels <harels@liveperson.com>
2017-05-16 17:34:44 +0300

    add widget for chronos / currier tests

---
+++ automation-custom-widget-sdk-navigate/script.js
@@ -0,0 +1,95 @@
+(function(){
+
+    var dataPaths = [
+        //"chattingAgentInfo.agentName",    TODO: these for should be given - under investigation
+        //"chattingAgentInfo.agentId",
+        //"chattingAgentInfo.agentGroupName",
+        //"chattingAgentInfo.agentNickname",
+        "agentInfo.accountId",
+        "agentInfo.agentName",
+        "agentInfo.agentId",
+        "agentInfo.agentNickname",
+        "agentInfo.agentEmail",
+        "agentInfo.maxChats",
+        "chatTranscript.lines",
+        "surveyQuestions.preChat.email.value",
+        "surveyQuestions.preChat.email.displayName",
+        "surveyQuestions.preChat.phone.value",
+        "surveyQuestions.preChat.phone.displayName",
+        "surveyQuestions.preChat.name.value",
+        "surveyQuestions.preChat.name.displayName",
+        "surveyQuestions.preChat.customizedQuestions",
+        "customVariables",
+        //"surveyQuestions.postChat",
+        //"surveyQuestions.agentSurvey",
+        "visitorInfo.visitorId",
+        "visitorInfo.visitorName",
+        "visitorInfo.device",
+        "visitorInfo.browser",
+        "visitorInfo.operatingSystem",
+        "visitorInfo.IpAddress",
+        "visitorInfo.visitStartTime",
+        "visitorInfo.chattingVisitorState",
+        "campaignInfo.campaignName",
+        "campaignInfo.campaignId",
+        "campaignInfo.campaignDescription",
+        "campaignInfo.targetAudience",
+        "campaignInfo.goalName",
+        "campaignInfo.goalId",
+        "campaignInfo.goalDescription",
+        "campaignInfo.lobName",
+        "campaignInfo.lobId",
+        "campaignInfo.lobDescription",
+        "engagementInfo.VisitorBehavior",
+        "engagementInfo.skill",
+        "engagementInfo.engagementType",
+        "engagementInfo.engagementId",
+        "engagementInfo.engagementName",
+        "engagementInfo.agentNote",
+        "SDE.customerDetails",
+        "SDE.personalInfo",
+        "SDE.marketingSource",
+        "SDE.leadGeneration",
+        "SDE.transaction",
+        "SDE.viewedProducts",
+        "SDE.shoppingCart",
+        "SDE.serviceActivity",
+        "SDE.error"
+    ];
+
+    window.onload = function() {
+        var resultDiv = document.getElementById('result_div'),
+            tableEl = document.getElementById('.table'),
+            result = 'OK';
+
+        resultDiv.setAttribute('data-attr',result);
+
+        lpTag.agentSDK.init({});
+
+        dataPaths.forEach(function(path){
+            lpTag.agentSDK.get(path,
+                function(data){
+                    var rowNode = document.createElement('tr');
+                    rowNode.innerHTML = "<td>" + path + "</td><td>" + (typeof data === 'object' ? JSON.stringify(data) : data) + "</td>";
+                    tableEl.appendChild(rowNode);
+                    if(!data || (data instanceof Array && !data.length)) {
+                        var errString = "[Missing: " + path + "]";
+                        result === 'OK' ? (result = errString) : (result += (", " + errString));
+                        rowNode.className += ' warning';
+                        resultDiv.setAttribute('data-attr',result);
+                    }
+                },
+                function(err){
+                    var rowNode = document.createElement('tr');
+                    rowNode.innerHTML = "<td>" + path + "</td><td>" + err + "</td>";
+                    tableEl.appendChild(rowNode);
+                    var errString = "[Error: " + path + "]";
+                    result === 'OK' ? (result = errString) : (result += (", " + errString));
+                    rowNode.className += ' warning';
+                    resultDiv.setAttribute('data-attr',result);
+                }
+            );
+        });
+    };
+
+})();
