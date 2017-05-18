9de76d95b3cc57457e1e9772902ff5607a0a5f7f
harels <harels@liveperson.com>
2017-05-18 10:52:54 +0300

    try with bind

--- automation-custom-widget-sdk-navigate/script.js
+++ automation-custom-widget-sdk-navigate/script.js
@@ -57,42 +57,87 @@
         "SDE.error"
     ];
 
-    window.refreshData = function () {
+    window.onload = function() {
         var resultDiv = document.getElementById('result_div'),
-            tableEl = document.getElementById('responseData'),
-            result = 'OK';
+            tableEl = document.getElementById('table'),
+            resultsObj = {
+                resultsOK: {},
+                resultsErr : {}
+            };
 
-        resultDiv.setAttribute('data-attr',result);
+        lpTag.agentSDK.init({});
 
         dataPaths.forEach(function(path){
-            lpTag.agentSDK.get(path,
+            lpTag.agentSDK.bind(path,
                 function(data){
-                    var rowNode = document.createElement('tr');
-                    rowNode.innerHTML = "<td>" + path + "</td><td>" + (typeof data === 'object' ? JSON.stringify(data) : data) + "</td>";
-                    tableEl.appendChild(rowNode);
-                    if(!data || (data instanceof Array && !data.length)) {
-                        var errString = "[Missing: " + path + "]";
-                        result === 'OK' ? (result = errString) : (result += (", " + errString));
-                        rowNode.className += ' warning';
-                        resultDiv.setAttribute('data-attr',result);
+
+                    if(!data.newValue || (data.newValue instanceof Array && !data.newValue.length)) {
+                        if(resultsObj.resultsOK[data.key]) {
+                            delete resultsObj.resultsOK[data.key];
+                        }
+                        var errString = "[Missing: " + data.key + "]";
+                        resultsObj.resultsErr[data.key] = errString;
+                    } else {
+                        if(resultsObj.resultsErr[data.key]) {
+                            delete resultsObj.resultsErr[data.key];
+                        }
+                        resultsObj.resultsOK[data.key] = data.newValue;
                     }
                 },
                 function(err){
-                    var rowNode = document.createElement('tr');
-                    rowNode.innerHTML = "<td>" + path + "</td><td>" + err + "</td>";
-                    tableEl.appendChild(rowNode);
-                    var errString = "[Error: " + path + "]";
-                    result === 'OK' ? (result = errString) : (result += (", " + errString));
-                    rowNode.className += ' warning';
-                    resultDiv.setAttribute('data-attr',result);
+                    if(err) {
+                        resultsObj.resultsErr['bindError'] = err;
+                    }
                 }
             );
         });
+
+        setInterval(redrawTable, 2000);
+
+        //write text to the chat input area
+        lpTag.agentSDK.command(lpTag.agentSDK.cmdNames.write, {text: "Sent by SDK..."});
+
+
+        function redrawTable() {
+
+            var rowNode,
+                path,
+                result;
+
+            tableEl.remove();
+            tableEl = document.createElement('table');
+            resultDiv.appendChild(tableEl);
+
+            rowNode = document.createElement('tr');
+            rowNode.innerHTML = "<th>Parameter</th><th>Value</td>";
+            tableEl.appendChild(rowNode);
+
+            for(path in resultsObj.resultsOK) {
+                rowNode = document.createElement('tr');
+                rowNode.innerHTML = "<td>" + path + "</td><td>" +
+                    (typeof resultsObj.resultsOK[path] === 'object' ? JSON.stringify(resultsObj.resultsOK[path]) : resultsObj.resultsOK[path]) + "</td>";
+                tableEl.appendChild(rowNode);
+            }
+
+            for(path in resultsObj.resultsErr) {
+                rowNode = document.createElement('tr');
+                rowNode.innerHTML = "<td>" + path + "</td><td>" + resultsObj.resultsErr[path] + "</td>";
+                rowNode.className += ' warning';
+                tableEl.appendChild(rowNode);
+            }
+
+            if(Object.keys(resultsObj.resultsErr).length) {
+                result = JSON.stringify(resultsObj.resultsErr);
+            } else {
+                var resultsLen = Object.keys(resultsObj.resultsOK).length;
+                result = (dataPaths.length === resultsLen) ?
+                    'OK' :
+                    (dataPaths.length - resultsLen + " not returned") ;
+            }
+
+            resultDiv.setAttribute('data-attr', result);
+
+        }
     };
 
-    window.onload = function() {
-        lpTag.agentSDK.init({});
-
-        window.refreshData();
-    };
 })();
