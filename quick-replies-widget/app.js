6461ef92719f4861aebacf725581a0b621be49cc
Amit Tal <amitt@liveperson.com>
2018-05-21 11:14:23 +0300

    remove amit-structure-content widget (playground) enhance quick-replies widget to include structured content possibility

--- quick-replies-widget/app.js
+++ quick-replies-widget/app.js
@@ -1,8 +1,12 @@
 (function (w, d, ns) {
 
     var sendBtn = d.getElementById('sendBtn');
-    var addTextBtn = d.getElementById('addTextBtn');
-    var addQuickRepliesBtn = d.getElementById('addQuickRepliesBtn');
+    var textRadioChosen = d.getElementById('text');
+    var jsonRadioChosen = d.getElementById('json');
+    var selectContentBtn = d.getElementById('selectContentBtn');
+    var contentOptions = d.getElementById('contentOptions');
+    var selectQuickRepliesBtn = d.getElementById('selectQuickRepliesBtn');
+    var quickRepliesOptions = d.getElementById('quickRepliesOptions');
     var clearLogBtn = d.getElementById('clearLogBtn');
     var increaseFontSizeBtn = d.getElementById('increaseFontSizeBtn');
     var decreaseFontSizeBtn = d.getElementById('decreaseFontSizeBtn');
@@ -17,9 +21,16 @@
 
     function init() {
 
+        ns.commands.populateContentListByType('text');
+        ns.commands.populateQuickReplies();
+
         bindCommand(sendBtn, 'send');
-        bindCommand(addTextBtn, 'addText');
-        bindCommand(addQuickRepliesBtn, 'addQuickReplies');
+        bindCommand(textRadioChosen, 'changeContentType');
+        bindCommand(jsonRadioChosen, 'changeContentType');
+        bindCommand(selectContentBtn, 'showContentOptionsList');
+        bindCommand(contentOptions, 'addContent');
+        bindCommand(selectQuickRepliesBtn, 'showQuickRepliesList');
+        bindCommand(quickRepliesOptions, 'addQuickReplies');
         bindCommand(clearLogBtn, 'clearLog');
         bindCommand(increaseFontSizeBtn, 'increaseFontSize');
         bindCommand(decreaseFontSizeBtn, 'decreaseFontSize');
--- quick-replies-widget/lib/commands.js
+++ quick-replies-widget/lib/commands.js
@@ -1,20 +1,27 @@
 (function (w, d, ns) {
 
     var html = d.getElementsByTagName('html')[0];
+    var contentOptions = d.getElementById('contentOptions');
     var outputLog = d.getElementById('outputLog');
-    var textInput = d.getElementById('textInput');
+    var contentInput = d.getElementById('contentInput');
     var quickRepliesInput = d.getElementById('quickRepliesInput');
+    var quickRepliesOptions = d.getElementById('quickRepliesOptions');
+    var contentTypeJson = d.getElementById('json');
 
     function send() {
+        if (!contentInput.value || contentInput.value === '') {
+            return;
+        }
 
+        var contentType = contentTypeJson && contentTypeJson.checked ? 'json' : 'text';
         var cmdName = lpTag.agentSDK.cmdNames.write;
         var data = {
-            text: textInput.value,
             quickReplies: quickRepliesInput.value,
             metadata: {
-                title: 'quick replies example',
+                title: 'quick replies example'
             }
         };
+        data[contentType] = contentInput.value;
 
         clearLog();
 
@@ -23,12 +30,55 @@
         });
     }
 
-    function addText() {
-        textInput.value = ns.data.text;
+    function changeContentType(e) {
+        populateContentListByType(e.target.id);
     }
 
-    function addQuickReplies() {
-        quickRepliesInput.value = ns.util.prettyPrint(ns.data.quickReplies);
+    function populateContentListByType(type) {
+        const contentList = [];
+        for (c in ns.data[type]) {
+            contentList.push('<div data-attr-type=' + type + ' data-attr-name=' + c + '>' + ns.data[type][c].title + '</div>');
+        }
+        contentOptions.innerHTML = contentList.join('');
+        contentOptions.onblur = function() { contentOptions.style.visibility = 'hidden'; };
+
+        // clear previous value
+        contentInput.value = '';
+    }
+
+    function populateQuickReplies() {
+        const quickRepliesList = [];
+        for (qr in ns.data.quickReplies) {
+            quickRepliesList.push('<div data-attr-name=' + qr + '>' + ns.data.quickReplies[qr].title + '</div>');
+        }
+        quickRepliesOptions.innerHTML = quickRepliesList.join('');
+        quickRepliesOptions.onblur = function() { quickRepliesOptions.style.visibility = 'hidden'; };
+    }
+
+    function showContentOptionsList() {
+        contentOptions.style.visibility = 'visible';
+        contentOptions.focus();
+    }
+
+    function addContent(e) {
+        const contentType = e.target.getAttribute('data-attr-type');
+        const contentKey = e.target.getAttribute('data-attr-name');
+        if (contentType && contentKey) {
+            var content = ns.data[contentType][contentKey].content;
+            contentInput.value = contentType === 'json' ? ns.util.prettyPrint(content) : content;
+        }
+    }
+
+    function showQuickRepliesList() {
+        quickRepliesOptions.style.visibility = 'visible';
+        quickRepliesOptions.focus();
+    }
+
+    function addQuickReplies(e) {
+        const quickRepliesKey = e.target.getAttribute('data-attr-name');
+        if (quickRepliesKey) {
+            quickRepliesInput.value = ns.util.prettyPrint(ns.data.quickReplies[quickRepliesKey].content);
+        }
     }
 
     function clearLog() {
@@ -47,7 +97,12 @@
 
 
     ns.commands = {
-        addText: addText,
+        populateQuickReplies: populateQuickReplies,
+        changeContentType: changeContentType,
+        populateContentListByType: populateContentListByType,
+        showContentOptionsList: showContentOptionsList,
+        addContent: addContent,
+        showQuickRepliesList: showQuickRepliesList,
         addQuickReplies: addQuickReplies,
         send: send,
         clearLog: clearLog,
